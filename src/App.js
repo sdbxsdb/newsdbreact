8dbe0a344deae774cd6e424392ef5fd85c47d3df
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-11-25 16:53:49 +0000

    react router dome and link added with working nav.  styling and content next

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from "react";
+
 import "./index.css";
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import { Route, Routes, Link } from 'react-router-dom';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="test">
-      <h1>asdf</h1>
+    <div className="bg-red-300">
+
+      <Nav />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/projects" element={<Projects />} />
+  
+        </Routes>
+
     </div>
   );
 }
 
-export default App;
+export default App; 
